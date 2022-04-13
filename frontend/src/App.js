6e0d865e7d24919b37051b0bf5df35f27324b59b
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2022-04-13 22:15:31 +0700

    Add initial files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,57 @@
+import React from "react";
+import "./index.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./views/homePage";
+import Login from "./views/loginPage";
+import Register from "./views/registerPage";
+import ProtectedPage from "./views/protectedPage";
+
 import logo from './logo.svg';
 import './App.css';
+
+
+function App1() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        <AuthProvider>
+          <Navbar />
+          <Switch>
+            <PrivateRoute component={ProtectedPage} path="/protected" exact />
+            <Route component={Login} path="/login" />
+            <Route component={Register} path="/register" />
+            <Route component={Home} path="/" />
+          </Switch>
+        </AuthProvider>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>MAQEIT</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React...
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App1;
