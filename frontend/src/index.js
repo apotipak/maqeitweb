e0c2eb465d88feba6f27ff37af1cc3138da91f6d
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2022-04-14 23:28:49 +0700

    Add initial files.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>
+);
+
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+*/
